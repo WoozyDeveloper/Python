68e72af7331b44f80078b9278512b0348f057f60
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-10-17 21:01:40 +0300

    6 in 2 moduri

--- lab 2/6.py
+++ lab 2/6.py
@@ -17,4 +17,25 @@ def find_x_times(my_lists, x):
     return ans
 
 
+def find_x_times2(x, *my_lists):
+    ans = list()
+    my_set = set()
+    for llist in my_lists:
+        for element in llist:
+            my_set.add(element)
+
+    counter = 0
+    for element in my_set:
+        for my_list in my_lists:
+            if element in my_list:
+                counter = counter + 1
+        #print(element, ' ', counter)
+        if counter == x:
+            ans.append(element)
+        counter = 0
+    return ans
+
+
 print(find_x_times([[1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"]], 2))
+
+print(find_x_times2(2, [1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"]))
