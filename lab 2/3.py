9006bb8594d212e0e005b93d5e748bf2eeff2ef7
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-10-20 18:09:56 +0300

    Update 3.py

--- lab 2/3.py
+++ lab 2/3.py
@@ -1,25 +1,25 @@
 def intersect(a, b):
-    ans = list()
-    for element in a:
-        if element in b:
-            ans.append(element)
-    return ans
+    # ans = list()
+    # for element in a:
+    #     if element in b:
+    #         ans.append(element)
+    return a & b
 
 
 def reunite(a, b):
-    ans = a
-    for element in b:
-        if element not in a:
-            ans.append(element)
-    return ans
+    # ans = a
+    # for element in b:
+    #     if element not in a:
+    #         ans.append(element)
+    return a | b
 
 
 def minus(a, b):
-    ans = list()
-    for element in a:
-        if element not in b:
-            ans.append(element)
-    return ans
+    # ans = list()
+    # for element in a:
+    #     if element not in b:
+    #         ans.append(element)
+    return a-b
 
 
 print(intersect([1, 2, 3], [3, 4, 5, 6]))
