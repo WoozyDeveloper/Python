05604c5b5dd170c841f520f96471e68e73ae73ec
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-10-26 13:26:51 +0300

    update tema 3

--- lab 3/3.py
+++ lab 3/3.py
@@ -1,11 +1,18 @@
 def f(a, b):
+    print(a.items())
+    print(b.keys())
+    print(b.values())
     for x in a.items():
-        if x[0] not in b.keys() or x[1] not in b.values():
+        l = []
+        l.append(b.get(x[0]))
+        if x[0] not in b.keys() or x[1] not in l:
             return False
     return True
 
 
-x = dict({"abc": [1, 1], "aaa": [2, 2]})
-y = dict({"abc": [1, 1], "aaa": [2, 2]})
+# x = dict({"a": 1, "b": 2, 1: {2: {3: 4}}})
+# y = dict({"a": 1, "b": 2, 1: {2: {3: 4}}})
+x = dict({"a": 1, "b": 2})
+y = dict({"a": 2, "b": 1})
 
 print(f(x, y))
