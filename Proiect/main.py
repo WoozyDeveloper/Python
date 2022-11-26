ef9084a552adf289a494ed9c4ad10a34f2b0ddf2
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-11-26 17:49:53 +0200

    Added the images of the cards + initialising the window with pygame
    - folder img with all the deck images
    - show blank window using pygame

---
+++ Proiect/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+(width, height) = (1920, 1080)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
